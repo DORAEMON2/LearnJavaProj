12927075f96d621efa976fc5ab5b6aa33ed2ea2e
Guo Linhui <DORAEMON2@users.noreply.github.com>
2018-07-03 21:36:24 +0800

    hellojava

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World! 2333");
+
+        String s = "";
+        for (int i = 0;i<10;i++){
+            s = s+String.valueOf(i);
+        }
+        System.out.println(s);
+    }
+}
